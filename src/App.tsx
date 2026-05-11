4c12177d8044cfd57b37d5a01ece21b0c27e97eb
Anna Viklund <annamariaviklund@gmail.com>
2026-05-11 14:51:53 +0200

    style: tweak background

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,22 @@ export function App() {
       <div className="fixed top-4 right-4 z-1 flex items-center gap-4">
         <ThemeToggle />
       </div>
-      <div className="border-b bg-muted p-6 py-12 md:px-16 md:py-8">
+      <div className="border-b p-6 py-12 md:px-16 md:py-8">
         <Content>
           <Header />
         </Content>
       </div>
-      <div className="p-6 md:p-16 md:pt-32">
+      <div className="bg-background p-6 md:p-16 md:pt-32">
         <Content>
           <InsectIpsum />
         </Content>
       </div>
-      <div className="p-6 md:p-16 md:pb-32">
+      <div className="border-b bg-background p-6 md:p-16 md:pb-32">
         <Content>
           <Photos />
         </Content>
       </div>
-      <div className="border-t bg-muted p-6 md:px-16 md:py-12">
+      <div className="p-6 md:px-16 md:py-12">
         <Content>
           <Footer />
         </Content>
